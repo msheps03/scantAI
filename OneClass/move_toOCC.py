d1ebbfb443e6a3dfb89d37e7e7d7033d7ee6158a
Maxwell Shepherd <32144191+msheps03@users.noreply.github.com>
2023-05-02 08:42:30 -0400

    visualize

--- OneClass/move_toOCC.py
+++ OneClass/move_toOCC.py
@@ -1,11 +1,11 @@
 import os
 import shutil
 
-
+'''
 path = '../new_dataset/'
 
 newPath = '../OneClass/Ticks/'
 for x in ['train/','validation/']:
     for i in os.listdir(path+x):
         print(i)
-        shutil.copy(path+x+i, newPath+x+i)
+        shutil.copy(path+x+i, newPath+x+i)'''
